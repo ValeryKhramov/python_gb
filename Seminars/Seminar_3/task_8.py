273da80fad697a14233355a9b57f09d6e780d1ec
Valery_Khramov <dr.hramoff2013@yandex.ru>
2024-01-21 09:47:04 +0300

    update: task_8 from the third seminar

--- Seminars/Seminar_3/task_8.py
+++ Seminars/Seminar_3/task_8.py
@@ -7,16 +7,44 @@
 # ✔ Для решения используйте операции с множествами.
 # Код должен расширяться на любое большее количество друзей.
 
-friends = {
-    'Афанасий': ('аккордеон', 'баян', 'велосипед', 'настроение'),
-    'Борис': ('аккордеон', 'баян', 'велосипед', 'настроение'),
-    'Владимир': ('аккордеон', 'баян', 'велосипед', 'розетка', 'бритва'),
-    }
+hike = {
+        'Aaz': ("спички", "спальник", "дрова", "топор"),
+        'Skeeve': ("спальник", "спички", "вода", "еда"),
+        'Tananda': ("вода", "спички", "косметичка"),
+}
 
-all_things = []
-for things in friends.values():
-    all_things.extend(things)
+temp_bag = None
+for things in hike.values():
+    if not temp_bag:
+        temp_bag = set(things)
+        continue
+    temp_bag = temp_bag.intersection(set(things))
+print(temp_bag)
 
-all_things_unit = set(all_things)
-# print(all_things_unit)
-)
+
+result_uniq = {}
+for name, things in hike.items():
+    temp = set(things)
+    temp_o = set()
+    for other in hike.values():
+        if things == other:
+            continue
+        temp_o = temp_o.union(set(other))
+    temp = temp.difference(temp_o)
+    if temp:
+        result_uniq.update({name: temp})
+print(result_uniq)
+
+
+res_missing = {}
+for name, things in hike.items():
+    temp = set(things)
+    for other in hike.values():
+        if things == other:
+            continue
+        temp_o = temp_o.intersection(set(other))
+    temp = temp_o.difference(temp)
+    if temp:
+        res_missing.update({name: temp})
+
+print(res_missing)
